85d10eae502b4778aaa4ff78b786241fc8ad1a14
Yelyzaveta Omelich <omelichelizaveta@gmail.com>
2025-06-29 13:16:26 +0300

    fix: module 2_2 add fallback transport

--- module 2_2/client/src/utils/socket.ts
+++ module 2_2/client/src/utils/socket.ts
@@ -4,7 +4,12 @@ const socket = io('http://localhost:4000', {
   auth: { userId: sessionStorage.getItem('userId') || null },
   reconnection: true,
   reconnectionAttempts: 5,
-  transports: ['websocket']
+  transports: ['websocket', 'polling'],
+});
+
+socket.on("connect_error", () => {
+  // revert to classic upgrade
+  socket.io.opts.transports = ["polling", "websocket"];
 });
 
 export default socket;
